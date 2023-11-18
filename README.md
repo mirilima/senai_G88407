79f8c320a6f0c7bf22110f1b461edfeb85c271a6
mirilima <miriellehermmings1@gmail.com>
2023-11-17 21:40:52 -0300

    commit exercicio01

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # senai_G88407
+# Mirelle Vitoria Alves Lima
+# miriellehermmings1@gmail.com
+# (71)987061587
---
+++ exercicio-01.py
@@ -0,0 +1,29 @@
+#nome = "Mirielle" #string
+#idade = 21 #inteiro
+#altura = 1.71 # numero de ponto flutuante(float)
+#estudante = True #boolean
+#
+#print(type(nome))
+#print(type(idade))
+#print(type(altura))
+#print(type(estudante))
+
+# operadores matematicos
+from pickle import FALSE
+
+
+soma = 5+3
+diferenca = 5-3
+produto= 5*3
+divisao = 5/3
+print("a soma é ",soma, "a diferença é ", diferenca,"o produto é ",produto,"a divisão é",divisao)
+# operadores de comparação
+igual = 5==3
+diferente =5!=3
+maior =5>3
+menor = 5<3
+print("igual é",FALSE,"diferenca é"True)
+#operadores logicos
+e = True and False
+ou = True or False
+Nao = not True
