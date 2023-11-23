ec9cc003bef77191b199e9b976082c736630c274
mirilima <miriellehermmings1@gmail.com>
2023-11-22 21:23:31 -0300

    commit exercicio-02

---
+++ exercicio-02.py
@@ -0,0 +1,28 @@
+idade = 20
+if idade <= 13:
+    print("voce é uma criança")
+if idade <18:
+        print("voce é um adolecente", idade)
+if idade >= 18:
+  print("voce é maior de idade", idade)
+else:
+  print("voce é uma adulto")
+
+
+  # boolean
+  genero = True 
+
+if genero != False:
+    print("feminino")
+else:
+    print("masculino")
+      
+status = True
+
+if status == True:
+   print("verdadeiro")
+else:
+    print("falso")
+
+
+     
