c1bb54c6fe8b1aa2046816974ed6224f3e406aaf
mirilima <miriellehermmings1@gmail.com>
2023-11-29 21:10:49 -0300

    commit exercicio-05.1

---
+++ exercicio-05.1.py
@@ -0,0 +1,13 @@
+usuarios ={
+    'usuario1': 'usuario1',
+    'usuario2': 'usuario22',
+    'usuario3': 'usuario33'
+    }
+usuario = input("insira seu usuario:\n")
+senha = input("insira sua senha:\n")
+
+if usuarios in usuarios and usuarios[usuario] == senha:
+    print(usuario)
+else:
+    print("usuario não existe, cadastre-se")
+while True:
