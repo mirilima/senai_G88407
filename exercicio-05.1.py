bc01c48a2cce2732b954bbb10af1a2719b1cdcf5
mirilima <miriellehermmings1@gmail.com>
2023-11-30 19:38:27 -0300

    commit exercicio-05.1

--- exercicio-05.1.py
+++ exercicio-05.1.py
@@ -1,13 +1,45 @@
-usuarios ={
-    'usuario1': 'usuario1',
-    'usuario2': 'usuario22',
-    'usuario3': 'usuario33'
-    }
+#usuarios
+usuarios ={}
 usuario = input("insira seu usuario:\n")
 senha = input("insira sua senha:\n")
 
-if usuarios in usuarios and usuarios[usuario] == senha:
-    print(usuario)
-else:
-    print("usuario não existe, cadastre-se")
+#cadastro de usuarios
+
+def novos_usuarios():
+    nome = input("Digite o nome de usuário: ")
+    senha = input("Digite a senha: ")
+    usuarios[nome] = senha
+    print("Usuário cadastrado com sucesso!")
+
+    # login
+
+def fazer_login():
+    nome = input("Digite o nome de usuário: ")
+    senha = input("Digite a senha: ")
+    if nome in usuarios and usuarios[nome] == senha:
+        print("Login bem-sucedido!")
+    else:
+        print("Nome de usuário ou senha incorretos.")
+
+        # menu principal
+        
 while True:
+    print("Menu Principal:")
+    print("1. Criar novo usuário")
+    print("2. Fazer login")
+    print("3. Sair")
+
+    escolha = input("Escolha uma opção: ")
+
+    if escolha == "1":
+        novos_usuarios()
+
+    elif escolha == "2":
+        fazer_login()
+
+    elif escolha == "3":
+        print("Saindo do programa. Até mais!")
+        break # para sair do loop
+
+    else:
+        print("Opção inválida. Escolha novamente.")
