cb35a51b84bd7677625ad63b0501873b60c19a1d
mirilima <miriellehermmings1@gmail.com>
2023-12-07 21:21:33 -0300

    commit exercicio-07

--- exercicio-07.py
+++ exercicio-07.py
@@ -1,10 +1,8 @@
-import pandas as PD
-dados ={
-    "nome":["maria","paulo","joão","rafa"],
-    "idade":[30,20,29,23],
-    "cidade":[ "camaçari","são paulo","santo andré", "são caetano do sul"]
-}
-df = PD.DataFrame(dados)
-for dado in df.values:
-      print(dado[0],dado[1])
+#import pandas as PD
+#dados ={
+   #"cidade":[ "camaçari","são paulo","santo andré", "são caetano do sul"]
+#df = PD.DataFrame(dados)
+#for dado in df.values:
+   #  print(dado[0],dado[1])
 
+import pandas as PD
